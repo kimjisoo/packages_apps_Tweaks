81b7fefca77cef609824c455f18f9d783fdc4433
Lars Greiss <kufikugel@googlemail.com>
2015-10-24 16:53:29 +0000

    Add reboot menu [2/2]
    - Adapted to Tweaks
    - Added a vector for the powermenu category
    
    -- Adaptions for N --
    
    - refator imports for preference package in support now
    - remove deprecated overrides
    
    Change-Id: I4d9b63926782a17b61da0c4e76090cf5ee065912
    
    And pimp it out [2/2]
    
    and turn advanced reboot on by default [2/2]
    
    Change-Id: I03fb0e5afbf37312493568125dfc95ace16ac67d
    
    Powermenu Torch option [2/2]
    
    Change-Id: Ia210f869b459495c85c6a1d95769558f175e0a53

---
+++ src/com/jisoo/tweaks/fragments/PowerMenu.java
@@ -0,0 +1,96 @@
+/*
+ * Copyright (C) 2014 The Dirty Unicorns Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.jisoo.tweaks.fragments;
+
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceCategory;
+import android.support.v7.preference.PreferenceScreen;
+import android.support.v7.preference.Preference.OnPreferenceChangeListener;
+import android.support.v14.preference.SwitchPreference;
+import android.provider.Settings;
+
+import com.android.settings.R;
+import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.internal.util.du.DuUtils;
+import com.android.settings.SettingsPreferenceFragment;
+import com.android.settings.Utils;
+
+public class PowerMenu extends SettingsPreferenceFragment implements OnPreferenceChangeListener {
+
+    private static final String KEY_ADVANCED_REBOOT = "advanced_reboot";
+    private static final String KEY_POWERMENU_TORCH = "powermenu_torch";
+
+    private ListPreference mAdvancedReboot;
+    private SwitchPreference mPowermenuTorch;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.powermenu);
+
+        final ContentResolver resolver = getActivity().getContentResolver();
+        final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mAdvancedReboot = (ListPreference) findPreference(KEY_ADVANCED_REBOOT);
+        mAdvancedReboot.setValue(String.valueOf(Settings.Secure.getInt(
+                getContentResolver(), Settings.Secure.ADVANCED_REBOOT, 1)));
+        mAdvancedReboot.setSummary(mAdvancedReboot.getEntry());
+        mAdvancedReboot.setOnPreferenceChangeListener(this);
+
+        mPowermenuTorch = (SwitchPreference) findPreference(KEY_POWERMENU_TORCH);
+        mPowermenuTorch.setOnPreferenceChangeListener(this);
+        if (!DuUtils.deviceSupportsFlashLight(getActivity())) {
+            prefScreen.removePreference(mPowermenuTorch);
+        } else {
+        mPowermenuTorch.setChecked((Settings.System.getInt(resolver,
+                Settings.System.POWERMENU_TORCH, 0) == 1));
+        }
+    }
+
+    @Override
+    protected int getMetricsCategory() {
+        return MetricsEvent.TWEAKS;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mAdvancedReboot) {
+            Settings.Secure.putInt(getContentResolver(), Settings.Secure.ADVANCED_REBOOT,
+                    Integer.valueOf((String) newValue));
+            mAdvancedReboot.setValue(String.valueOf(newValue));
+            mAdvancedReboot.setSummary(mAdvancedReboot.getEntry());
+        } else if (preference == mPowermenuTorch) {
+            boolean checked = ((SwitchPreference)preference).isChecked();
+            Settings.System.putInt(getActivity().getContentResolver(),
+                    Settings.System.POWERMENU_TORCH, checked ? 1:0);
+            return true;
+        }
+        return false;
+    }
+}
